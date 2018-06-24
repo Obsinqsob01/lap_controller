2c81dc80c406098bca4e2eb37adcce6f20bf3f58
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-06-24 13:10:07 -0500

    Added the bulma css file

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ def index():
 @app.route('/user/<username>')
 def user(username):
     return render_template('user.html', username=username)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+    
