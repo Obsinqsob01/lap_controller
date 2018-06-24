f9cb2c2e4a78d8cd37bfbe5da2e761b9c78eaa6f
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-06-24 12:32:50 -0500

    Added the route and file for the user

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/hello')
-def hello():
-    return "Hello page"
+@app.route('/user/<username>')
+def user(username):
+    return render_template('user.html', username=username)
