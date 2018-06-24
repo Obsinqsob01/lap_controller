11a45e9daf14cf33d5126163007c2f1177f7da13
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-06-24 12:25:59 -0500

    Deleted the deadline of static files

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    url_for('static', filename='style.css')
-
     return render_template('index.html')
 
 @app.route('/hello')
 def hello():
-    url_for('static', filename='style.css')
-
     return "Hello page"
