706c69c7ec1d69ef032b5a64676c9a68cb0a65d1
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-06-24 13:13:05 -0500

    Removed the user route, template and extends other

--- main.py
+++ main.py
@@ -6,10 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/user/<username>')
-def user(username):
-    return render_template('user.html', username=username)
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-    
