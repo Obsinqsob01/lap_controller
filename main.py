6e3f1582b044960d369d83df2e2868364c61f5d0
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-06-24 12:24:20 -0500

    Static file css and index.html file created

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hola mundo!"
+def index():
+    url_for('static', filename='style.css')
+
+    return render_template('index.html')
 
 @app.route('/hello')
 def hello():
+    url_for('static', filename='style.css')
+
     return "Hello page"
