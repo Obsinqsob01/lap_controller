5e4ec4980ef13a8309363f97adc80c177f99e196
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-06-24 13:28:39 -0500

    send file route, template and some extra style

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/send-file', methods=['POST', 'GET'])
+def send_file():
+    if request.method == 'POST':
+        print("pst method")
+    elif request.method == 'GET':
+        return render_template('send-file.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
